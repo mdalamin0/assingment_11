29df077e6367d15d139ce0723097febe0445a27b
md al-amin <muhammadalamin809@gmail.com>
2023-05-17 23:56:56 +0800

    NavBar done

---
+++ src/Pages/Blog/Blog.jsx
@@ -0,0 +1,11 @@
+
+
+const Blog = () => {
+    return (
+        <div>
+            <h3>This is Blog</h3>
+        </div>
+    );
+};
+
+export default Blog;
--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main/Main";
 import Home from "../Pages/Home/Home/Home";
+import Blog from "../Pages/Blog/Blog";
+import AllToys from "../Pages/AllToys/AllToys";
 
 const router = createBrowserRouter([
     {
@@ -10,6 +12,14 @@ const router = createBrowserRouter([
         {
             path: '/',
             element: <Home></Home>
+        },
+        {
+            path: 'allToys',
+            element: <AllToys></AllToys>
+        },
+        {
+            path: 'blog',
+            element: <Blog></Blog>
         }
       ]
     },
